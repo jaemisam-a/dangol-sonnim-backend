fff0cf85eab8c4bcca48fbe7ae1083859598238c
Marine <rnjstmdals6@naver.com>
2023-02-12 03:48:14 +0900

    chore: @ExtendWith(MockitoExtension.class) 어노테이션 추가
     JUNit 5에 Mockito Extension을 사용하여 테스트를 확장하도록 지시합니다.
    
    Mockito를 사용하는 데 필요한 인프라를 제공하는데 Mock 을 만들고 스텁을 설정하고 Mock 간의 상호작용을 검증합니다.

--- src/test/java/com/dangol/dangolsonnimbackend/boss/service/impl/BossServiceImplTest.java
+++ src/test/java/com/dangol/dangolsonnimbackend/boss/service/impl/BossServiceImplTest.java
@@ -1,7 +1,11 @@
 package com.dangol.dangolsonnimbackend.boss.service.impl;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class BossServiceImplTest {
 
 }
