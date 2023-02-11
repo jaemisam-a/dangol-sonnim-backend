b8e080b0af4519417f2e589dde6eb6527972983b
Marine <rnjstmdals6@naver.com>
2023-02-12 01:53:58 +0900

    feat: ErrorCode 추가
    "메시지" 필드를 사용하여 오류 코드의 메세지를 열거형으로 정의합니다.

---
+++ src/main/java/com/dangol/dangolsonnimbackend/errors/enumeration/ErrorCode.java
@@ -0,0 +1,12 @@
+package com.dangol.dangolsonnimbackend.errors.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    ;
+
+    private String message;
+}
