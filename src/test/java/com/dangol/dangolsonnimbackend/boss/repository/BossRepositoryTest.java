14c058358ed3935dfa6afa709e44a24e9ea503f6
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-12 09:45:16 +0900

    feat: add @AutoConfigureTestDatabase annotation for test
    [issue-key] DS-36
    - 테스트 시 H2 데이터베이스를 사용하지 않고 MySQL 데이터베이스를 사용하도록 설정
    - Reference: 스프링 부트 핵심가이드 p.192~193

--- src/test/java/com/dangol/dangolsonnimbackend/boss/repository/BossRepositoryTest.java
+++ src/test/java/com/dangol/dangolsonnimbackend/boss/repository/BossRepositoryTest.java
@@ -5,6 +5,8 @@ import com.dangol.dangolsonnimbackend.boss.dto.BossSignupRequestDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
@@ -12,6 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class BossRepositoryTest {
 
     @Autowired
