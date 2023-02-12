cf0f173ae42aee8fd0b8be0bbcd77307c8a6a809
Marine <rnjstmdals6@naver.com>
2023-02-13 04:35:04 +0900

    feat: RestApiExceptionHandler의 반환 객체 수정
    ApiResponse가 삭제됨에 따라 ResponseEntity를 반환하도록 수정
    
    오류 메시지와 요청 ID를 캡슐화하는 별도의 오류 응답 클래스 작성

--- src/main/java/com/dangol/dangolsonnimbackend/errors/RestApiExceptionHandler.java
+++ src/main/java/com/dangol/dangolsonnimbackend/errors/RestApiExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.dangol.dangolsonnimbackend.errors;
 
-import com.dangol.dangolsonnimbackend.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,8 +12,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class RestApiExceptionHandler {
 
     @ExceptionHandler(value = {BadRequestException.class})
-    public ApiResponse<Object> handleBadRequestException(BadRequestException ex) {
-        log.info("!!!Bad Request Error, [{}], message={}", MDC.get("UUID"), ex.getMessage());
-        return ApiResponse.clientFail("message", ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
+        // MDC 는 에러 로그를 식별하기 위해 사용됩니다.
+        String requestId = MDC.get("UUID");
+        log.info("Bad Request Error, requestId={}, message={}", requestId, ex.getMessage());
+
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), requestId);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
+
+
+class ErrorResponse {
+    private String message;
+    private String requestId;
+
+    public ErrorResponse(String message, String requestId) {
+        this.message = message;
+        this.requestId = requestId;
     }
 }
