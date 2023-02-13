4788c0b0cdf27ecc647b4fd161074af800699873
Marine <rnjstmdals6@naver.com>
2023-02-14 04:11:10 +0900

    feat: @DataJpaTest 에서 @SpringBootTest 로 전환
    단위 테스트가 아닌 통합 테스트로 해당 클래스에 대한 테스트를 진행합니다. 여기서 @Transactional 이란 메소드 내에서 데이터베이스에 대한 변경사항은 메소드가 완료된 후 롤백되어 데이터베이스가 원래 상태로 유지됩니다. 각 테스트 케이스가 다른 테스트 케이스와 독립적으로 실행되고 데이터베이스 상태가 다른 테스트 케이스보다 먼저 실행되는 테스트 케이스의 영향을 받지 않도록 하기 위해 사용했습니다!

--- src/test/java/com/dangol/dangolsonnimbackend/boss/repository/BossRepositoryTest.java
+++ src/test/java/com/dangol/dangolsonnimbackend/boss/repository/BossRepositoryTest.java
@@ -8,13 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@SpringBootTest
+@Transactional
 class BossRepositoryTest {
 
     @Autowired
