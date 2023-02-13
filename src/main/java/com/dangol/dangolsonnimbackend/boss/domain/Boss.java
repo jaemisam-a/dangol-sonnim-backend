7effd60a42204462d9b8b7edb67a5a542d0a4b56
Marine <rnjstmdals6@naver.com>
2023-02-13 18:40:48 +0900

    feat: 사장님 폰 번호 유니크 키 설정 해제
    사장님은 사업장이 여러 개 일 수 있고, 그에 맞춰 계정도 여러 개가 될 수 있기 때문에 폰 번호는 중복 될 수 있다.

--- src/main/java/com/dangol/dangolsonnimbackend/boss/domain/Boss.java
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/domain/Boss.java
@@ -29,7 +29,7 @@ public class Boss {
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String bossPhoneNumber;
 
     @Column(nullable = false, unique = true)
