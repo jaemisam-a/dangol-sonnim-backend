77f77bdd2f866686efb4906dfdc797600a099399
Marine <rnjstmdals6@naver.com>
2023-02-14 18:41:43 +0900

    Revert "[DS-36] 사장님 Repository 추가"

--- src/main/java/com/dangol/dangolsonnimbackend/boss/repository/BossRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dangol.dangolsonnimbackend.boss.repository;
-
-import com.dangol.dangolsonnimbackend.boss.domain.Boss;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BossRepository extends JpaRepository<Boss, Long> {
-}
