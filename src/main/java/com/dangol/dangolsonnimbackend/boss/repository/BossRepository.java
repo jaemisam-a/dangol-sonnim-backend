48925b0c1ca00c845ecc1112f19f56e5faab37aa
Marine <rnjstmdals6@naver.com>
2023-02-14 20:13:13 +0900

    fix: 이전 브랜치 기반 클래스 파일 재생성

---
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/repository/BossRepository.java
@@ -0,0 +1,7 @@
+package com.dangol.dangolsonnimbackend.boss.repository;
+
+import com.dangol.dangolsonnimbackend.boss.domain.Boss;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BossRepository extends JpaRepository<Boss, Long> {
+}
