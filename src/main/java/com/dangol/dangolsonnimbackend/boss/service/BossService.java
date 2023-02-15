6d650807bdda582a64b23a2614b68b5334941116
Marine <rnjstmdals6@naver.com>
2023-02-16 03:48:26 +0900

    feat: 이전 PR 내용 업로드

---
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/service/BossService.java
@@ -0,0 +1,8 @@
+package com.dangol.dangolsonnimbackend.boss.service;
+
+import com.dangol.dangolsonnimbackend.boss.dto.BossSignupRequestDTO;
+
+public interface BossService {
+
+    void signup(BossSignupRequestDTO dto);
+}
