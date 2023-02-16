4855a2e73b7c4a7342535a15666a884eca45ba2a
Marine <rnjstmdals6@naver.com>
2023-02-17 01:35:03 +0900

    feat: 사장님 로그인 응답 DTO
    액세스 토큰을 응답함.

---
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/dto/BossSigninResponseDTO.java
@@ -0,0 +1,10 @@
+package com.dangol.dangolsonnimbackend.boss.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BossSigninResponseDTO {
+    private String accessToken;
+}
