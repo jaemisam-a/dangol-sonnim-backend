1ede37dc9cceba72f323e42fd23740e0541c6421
Marine <rnjstmdals6@naver.com>
2023-02-18 01:02:30 +0900

    refact: 필요없는 import 제거

--- src/main/java/com/dangol/dangolsonnimbackend/boss/dto/BossSignupRequestDTO.java
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/dto/BossSignupRequestDTO.java
@@ -1,12 +1,9 @@
 package com.dangol.dangolsonnimbackend.boss.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
