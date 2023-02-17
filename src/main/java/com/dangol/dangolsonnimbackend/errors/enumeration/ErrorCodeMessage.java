4132907635e175270b13761139da8f1c371f52b0
Marine <rnjstmdals6@naver.com>
2023-02-18 01:10:56 +0900

    feat: 에러코드 추가

--- src/main/java/com/dangol/dangolsonnimbackend/errors/enumeration/ErrorCodeMessage.java
+++ src/main/java/com/dangol/dangolsonnimbackend/errors/enumeration/ErrorCodeMessage.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCodeMessage {
-    ALREADY_EXISTS_STORE_REGISTER_NUMBER("이미 존재하는 사업자 등록 번호입니다.");
+    ALREADY_EXISTS_STORE_REGISTER_NUMBER("이미 존재하는 사업자 등록 번호입니다."),
+    PASSWORD_NOT_MATCH("해당 비밀번호가 일치하지 않습니다."),
+    BOSS_NOT_FOUND("해당 이메일을 가진 사장님이 존재하지 않습니다.");
 
     private String message;
 }
