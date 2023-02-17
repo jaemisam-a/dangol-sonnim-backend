4ff7dd999a63c34e8b04a2ee8e442e43f436dd27
Marine <rnjstmdals6@naver.com>
2023-02-18 01:31:42 +0900

    feat: BossService 자격증명 가져오기 기능 추가
    dto의 email 을 통해 사장님을 찾는다. 없으면 400 번 에러를 던진다
    
    password 와 dto의 password의 일치 여부를 확인한다. 마찬가지로 일치하지 않으면 400번 에러를 던진다.
    
    이후 이메일 기반으로 토큰을 생성하고 responseDTO 에 담아 반환한다.

--- src/main/java/com/dangol/dangolsonnimbackend/boss/service/BossService.java
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/service/BossService.java
@@ -1,8 +1,11 @@
 package com.dangol.dangolsonnimbackend.boss.service;
 
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninReqeustDTO;
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninResponseDTO;
 import com.dangol.dangolsonnimbackend.boss.dto.BossSignupRequestDTO;
 
 public interface BossService {
 
     void signup(BossSignupRequestDTO dto);
+    BossSigninResponseDTO getByCredentials(BossSigninReqeustDTO dto);
 }
--- src/main/java/com/dangol/dangolsonnimbackend/boss/service/impl/BossServiceImpl.java
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/service/impl/BossServiceImpl.java
@@ -1,16 +1,20 @@
 package com.dangol.dangolsonnimbackend.boss.service.impl;
 
 import com.dangol.dangolsonnimbackend.boss.domain.Boss;
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninReqeustDTO;
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninResponseDTO;
 import com.dangol.dangolsonnimbackend.boss.dto.BossSignupRequestDTO;
 import com.dangol.dangolsonnimbackend.boss.repository.BossRepository;
 import com.dangol.dangolsonnimbackend.boss.repository.dsl.BossQueryRepository;
 import com.dangol.dangolsonnimbackend.boss.service.BossService;
+import com.dangol.dangolsonnimbackend.config.jwt.TokenProvider;
 import com.dangol.dangolsonnimbackend.errors.BadRequestException;
 import com.dangol.dangolsonnimbackend.errors.enumeration.ErrorCodeMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class BossServiceImpl implements BossService {
@@ -18,12 +22,14 @@ public class BossServiceImpl implements BossService {
     private final BossRepository bossRepository;
     private final BossQueryRepository bossQueryRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
 
     public BossServiceImpl(BossRepository bossRepository, BossQueryRepository bossQueryRepository,
-                           PasswordEncoder passwordEncoder){
+                           PasswordEncoder passwordEncoder, TokenProvider tokenProvider){
         this.bossRepository = bossRepository;
         this.bossQueryRepository = bossQueryRepository;
         this.passwordEncoder = passwordEncoder;
+        this.tokenProvider = tokenProvider;
     }
 
     @Transactional
@@ -32,6 +38,18 @@ public class BossServiceImpl implements BossService {
 
         dto.passwordEncode(passwordEncoder.encode(dto.getPassword()));
         bossRepository.save(new Boss(dto));
+    }
+
+    @Transactional
+    public BossSigninResponseDTO getByCredentials(BossSigninReqeustDTO reqeustDTO) {
+        Boss boss = Optional.ofNullable(bossQueryRepository.findByEmail(reqeustDTO.getEmail())).orElseThrow(
+                () -> new BadRequestException(ErrorCodeMessage.BOSS_NOT_FOUND));
+        if(!passwordEncoder.matches(reqeustDTO.getPassword(), boss.getPassword())){
+            throw new BadRequestException(ErrorCodeMessage.PASSWORD_NOT_MATCH);
+        }
+
+        final String token = tokenProvider.generateAccessToken(reqeustDTO.getEmail());
+        return new BossSigninResponseDTO(token);
     }
 
     private void validateSignup(BossSignupRequestDTO dto) {
