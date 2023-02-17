0993267124abbd770f168dcd1349152c69c0d2ee
Marine <rnjstmdals6@naver.com>
2023-02-18 02:18:49 +0900

    fix: 실수로 머지해서 사라진 부분 다시 복구

--- src/main/java/com/dangol/dangolsonnimbackend/boss/service/impl/BossServiceImpl.java
+++ src/main/java/com/dangol/dangolsonnimbackend/boss/service/impl/BossServiceImpl.java
@@ -1,10 +1,13 @@
 package com.dangol.dangolsonnimbackend.boss.service.impl;
 
 import com.dangol.dangolsonnimbackend.boss.domain.Boss;
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninReqeustDTO;
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninResponseDTO;
 import com.dangol.dangolsonnimbackend.boss.dto.BossSignupRequestDTO;
 import com.dangol.dangolsonnimbackend.boss.repository.BossRepository;
 import com.dangol.dangolsonnimbackend.boss.repository.dsl.BossQueryRepository;
 import com.dangol.dangolsonnimbackend.boss.service.BossService;
+import com.dangol.dangolsonnimbackend.config.jwt.TokenProvider;
 import com.dangol.dangolsonnimbackend.errors.BadRequestException;
 import com.dangol.dangolsonnimbackend.errors.enumeration.ErrorCodeMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,12 +23,14 @@ public class BossServiceImpl implements BossService {
     private final BossRepository bossRepository;
     private final BossQueryRepository bossQueryRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
 
     public BossServiceImpl(BossRepository bossRepository, BossQueryRepository bossQueryRepository,
-                           PasswordEncoder passwordEncoder){
+                           PasswordEncoder passwordEncoder, TokenProvider tokenProvider){
         this.bossRepository = bossRepository;
         this.bossQueryRepository = bossQueryRepository;
         this.passwordEncoder = passwordEncoder;
+        this.tokenProvider = tokenProvider;
     }
 
     @Transactional
@@ -41,6 +46,18 @@ public class BossServiceImpl implements BossService {
         return Optional.ofNullable(bossQueryRepository.findByEmail(email)).orElseThrow(
                 () -> new BadRequestException(ErrorCodeMessage.BOSS_NOT_FOUND)
         );
+    }
+
+    @Transactional
+    public BossSigninResponseDTO getByCredentials(BossSigninReqeustDTO reqeustDTO) {
+        Boss boss = Optional.ofNullable(bossQueryRepository.findByEmail(reqeustDTO.getEmail())).orElseThrow(
+                () -> new BadRequestException(ErrorCodeMessage.BOSS_NOT_FOUND));
+        if (!passwordEncoder.matches(reqeustDTO.getPassword(), boss.getPassword())) {
+            throw new BadRequestException(ErrorCodeMessage.PASSWORD_NOT_MATCH);
+        }
+
+        final String token = tokenProvider.generateAccessToken(reqeustDTO.getEmail());
+        return new BossSigninResponseDTO(token);
     }
 
     private void validateSignup(BossSignupRequestDTO dto) {
