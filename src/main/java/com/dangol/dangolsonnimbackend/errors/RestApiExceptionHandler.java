ce30a09956ac60217e64e2005dc5b59d0b87a1a0
Marine <rnjstmdals6@naver.com>
2023-02-20 00:49:10 +0900

    fix: 에러 핸들링 작동 오류 수정
    처음 예외가 던져졌을 때 400번으로 예상된 상태코드가 500으로 던져졌습니다.
    ErrorResponse 에 @Getter 를 추가해주니 정상적으로 400번으로 상태코드가 반환되었습니다.
    
    또 reqeustId 가 null로 반환되는 것을 확인하였습니다. MDC로 reqeustId를 생성하려고 했지만 따로 MDC를 저장하는 인터셉터 구현이 필요하다는 것을 알게 되었습니다.
    
    결론적으로는 UUID로 requestId를 간단하게 생성하여 넣어주었습니다.

--- src/main/java/com/dangol/dangolsonnimbackend/errors/RestApiExceptionHandler.java
+++ src/main/java/com/dangol/dangolsonnimbackend/errors/RestApiExceptionHandler.java
@@ -1,33 +1,32 @@
 package com.dangol.dangolsonnimbackend.errors;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.UUID;
 
 @RestControllerAdvice
 @Slf4j
 public class RestApiExceptionHandler {
 
     @ExceptionHandler(value = {BadRequestException.class})
-    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
-        // MDC 는 에러 로그를 식별하기 위해 사용됩니다.
-        String requestId = MDC.get("UUID");
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
+        // requestId 는 에러 로그를 식별하기 위해 사용됩니다.
+        String requestId = UUID.randomUUID().toString();
         log.info("Bad Request Error, requestId={}, message={}", requestId, ex.getMessage(), ex);
 
         ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), requestId);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
-
+    @Getter
+    @AllArgsConstructor
     private class ErrorResponse {
         private String message;
         private String requestId;
-
-        public ErrorResponse(String message, String requestId) {
-            this.message = message;
-            this.requestId = requestId;
-        }
     }
 }
