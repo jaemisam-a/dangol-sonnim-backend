c4c375c777232d6477fa4d23568652e53c690984
Marine <rnjstmdals6@naver.com>
2023-02-20 00:50:53 +0900

    test: 사장님 로그인 관련 테스트 추가

--- src/test/java/com/dangol/dangolsonnimbackend/boss/controller/BossControllerTest.java
+++ src/test/java/com/dangol/dangolsonnimbackend/boss/controller/BossControllerTest.java
@@ -1,11 +1,11 @@
 package com.dangol.dangolsonnimbackend.boss.controller;
 
 import com.dangol.dangolsonnimbackend.boss.domain.Boss;
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninReqeustDTO;
+import com.dangol.dangolsonnimbackend.boss.dto.BossSigninResponseDTO;
 import com.dangol.dangolsonnimbackend.boss.dto.BossSignupRequestDTO;
-import com.dangol.dangolsonnimbackend.boss.repository.dsl.BossQueryRepository;
 import com.dangol.dangolsonnimbackend.boss.service.BossService;
-import com.dangol.dangolsonnimbackend.errors.BadRequestException;
-import com.dangol.dangolsonnimbackend.errors.enumeration.ErrorCodeMessage;
+import com.dangol.dangolsonnimbackend.config.jwt.TokenProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import javax.transaction.Transactional;
 
@@ -27,10 +28,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BossControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
     private BossService bossService;
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void givenSignupDto_whenSignup_thenCreateNewBoss() throws Exception {
@@ -50,5 +52,60 @@ class BossControllerTest {
         Boss boss = bossService.findByEmail(dto.getEmail());
         assertNotNull(boss);
         assertEquals(dto.getName(), boss.getName());
+    }
+
+    @Test
+    void givenValidCredentials_whenAuthenticate_thenSucceed() throws Exception {
+        // given
+        BossSignupRequestDTO dto = new BossSignupRequestDTO();
+        dto.setName("Test Boss");
+        dto.setEmail("test@example.com");
+        dto.setPassword("password");
+        dto.setPhoneNumber("01012345678");
+        dto.setMarketingAgreement(true);
+        bossService.signup(dto);
+
+        BossSigninReqeustDTO requestDTO = new BossSigninReqeustDTO("test@example.com", "password");
+
+        // when
+        MvcResult result = mockMvc.perform(
+                        post("/api/v1/boss/signin")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(requestDTO))
+                                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                ).andExpect(status().isOk())
+                .andReturn();
+
+        // then
+        String contentAsString = result.getResponse().getContentAsString();
+        BossSigninResponseDTO responseDTO = new ObjectMapper().readValue(contentAsString, BossSigninResponseDTO.class);
+        String token = responseDTO.getAccessToken();
+        assertNotNull(token);
+        String getEmail = tokenProvider.getEmailFromToken(token);
+        assertEquals("test@example.com", getEmail);
+    }
+
+    @Test
+    void givenInvalidCredentials_whenAuthenticate_thenFail() throws Exception {
+        // given
+        BossSignupRequestDTO dto = new BossSignupRequestDTO();
+        dto.setName("Test Boss");
+        dto.setEmail("test@example.com");
+        dto.setPassword("password");
+        dto.setPhoneNumber("01012345678");
+        dto.setMarketingAgreement(true);
+        bossService.signup(dto);
+
+        BossSigninReqeustDTO requestDTO = new BossSigninReqeustDTO("test@example.com", "invalid-password");
+
+        // when
+        mockMvc.perform(
+                        post("/api/v1/boss/signin")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(requestDTO))
+                                .with(SecurityMockMvcRequestPostProcessors.csrf())
+                )
+                // then
+                .andExpect(status().isBadRequest());
     }
 }
