77a15bbf75e22382486ce6e6e73350d3397df490
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-02-19 09:05:25 +0900

    test: check to convert Store dto to entity class
    [issue-key] DS-45
    - 가게 DTO 클래스로부터 엔티티 클래스가 정상적으로 변환되었는지 확인
    - 빌더 패턴을 활용하여 DTO 인스턴스 생성

--- src/test/java/com/dangol/dangolsonnimbackend/store/domain/StoreTest.java
+++ src/test/java/com/dangol/dangolsonnimbackend/store/domain/StoreTest.java
@@ -11,15 +11,20 @@ public class StoreTest {
     @Test
     @DisplayName("가게 DTO 클래스로부터 엔티티 클래스가 정상적으로 생성이 되야 한다.")
     void testStoreSignupRequestDTOtoStoreConversion() {
-        StoreSignupRequestDTO dto = new StoreSignupRequestDTO();
-        dto.setName("단골손님");
-        dto.setStorePhoneNumber("01012345678");
-        dto.setNewAddress("서울특별시 서초구 단골로 130");
-        dto.setSido("서울특별시");
-        dto.setSigungu("서초구");
-        dto.setBname1("단골동");
-        dto.setComments("단골손님 가게로 좋아요.");
-        dto.setOfficeHours("08:00~10:00");
+        StoreSignupRequestDTO dto = StoreSignupRequestDTO.builder()
+                .name("단골손님")
+                .storePhoneNumber("01012345678")
+                .newAddress("서울특별시 서초구 단골로 130")
+                .sido("서울특별시")
+                .sigungu("서초구")
+                .bname1("단골동")
+                .detailedAddress("")
+                .comments("단골손님 가게로 좋아요.")
+                .officeHours("08:00~10:00")
+                .categoryId(1L)
+                .storeRegisterNumber("123-456-789")
+                .storeRegisterName("단골손님")
+                .build();
 
         Store store = new Store(dto);
 
@@ -32,5 +37,7 @@ public class StoreTest {
         assertNull(dto.getBname2());
         assertEquals(dto.getComments(), store.getComments());
         assertEquals(dto.getOfficeHours(), store.getOfficeHours());
+        assertEquals(dto.getStoreRegisterNumber(), store.getStoreRegisterNumber());
+        assertEquals(dto.getStoreRegisterName(), store.getStoreRegisterName());
     }
 }
