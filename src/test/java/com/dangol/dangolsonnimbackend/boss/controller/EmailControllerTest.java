020d4ede8c7178093082ff611f5800b566cb58cb
Marine <rnjstmdals6@naver.com>
2023-02-23 03:19:34 +0900

    test: 이메일 인증 테스트
    MockTest 이기 때문에 실제 이메일 발송은 하지 않는다.
    
    해당 테스트는 이메일 발송을 6회 간 진행하는데 총 64734ms가 걸립니다.
    
    전체 테스트 과정에서 시간지연이 발생하기 때문에 임시 주석 처리했습니다.
    
    이후 비동기 설정 후 처리해야 할 부분입니다.
    
    테스트는 성공합니다.

---
+++ src/test/java/com/dangol/dangolsonnimbackend/boss/controller/EmailControllerTest.java
@@ -0,0 +1,63 @@
+//package com.dangol.dangolsonnimbackend.boss.controller;
+//
+//import com.dangol.dangolsonnimbackend.boss.dto.AuthCodeRequestDTO;
+//import com.dangol.dangolsonnimbackend.boss.dto.AuthCodeResponseDTO;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//
+//import javax.transaction.Transactional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//@Transactional
+//class EmailControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    public void givenEmail_whenSendAuthCode_thenSuccess() throws Exception {
+//        long startTime = System.currentTimeMillis();
+//
+//        // given
+//        String email = "test@example.com";
+//        AuthCodeRequestDTO requestDTO = new AuthCodeRequestDTO();
+//        requestDTO.setEmail(email);
+//
+//        // when 최대 허용 요청 횟수 이내의 요청인 경우
+//        for (int i = 0; i < 5; i++) {
+//            MvcResult result = mockMvc.perform(post("/api/v1/email/send-auth-code")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(new ObjectMapper().writeValueAsString(requestDTO))
+//                        .with(csrf())
+//                    )
+//                    .andExpect(status().isOk())
+//                    .andReturn();
+//
+//            // then
+//            String contentAsString = result.getResponse().getContentAsString();
+//            AuthCodeResponseDTO responseDTO = new ObjectMapper().readValue(contentAsString, AuthCodeResponseDTO.class);
+//            assertNotNull(responseDTO.getAuthCode());
+//        }
+//        // when 최대 허용 요청 횟수를 초과한 경우
+//        mockMvc.perform(post("/api/v1/email/send-auth-code")
+//                        .with(csrf())
+//                        .param("email", email))
+//                // then
+//                .andExpect(status().isBadRequest());
+//
+//        long endTime = System.currentTimeMillis();
+//        System.out.println(String.format("코드 실행 시간: %20dms", endTime - startTime));
+//    }
+//}
