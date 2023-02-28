7a36ba03bc774b9aa60fc387627ac56cdd7b35cd
Marine <rnjstmdals6@naver.com>
2023-02-28 21:30:06 +0900

    feat: 구독권 생성 서비스 생성
    create : 구독권 생성
    classify : 구독권 타입에 따라 분류
    toResponseDTO : 구독권 타입에 따라 응답 DTO 생성 후 반환

--- src/main/java/com/dangol/dangolsonnimbackend/subscribe/service/SubscribeService.java
+++ src/main/java/com/dangol/dangolsonnimbackend/subscribe/service/SubscribeService.java
@@ -1,4 +1,12 @@
 package com.dangol.dangolsonnimbackend.subscribe.service;
 
+import com.dangol.dangolsonnimbackend.store.domain.Store;
+import com.dangol.dangolsonnimbackend.subscribe.domain.Subscribe;
+import com.dangol.dangolsonnimbackend.subscribe.dto.SubscribeRequestDTO;
+import com.dangol.dangolsonnimbackend.subscribe.dto.SubscribeResponseDTO;
+
 public interface SubscribeService {
+    SubscribeResponseDTO create(SubscribeRequestDTO dto);
+    Subscribe classify(SubscribeRequestDTO dto, Store store);
+    SubscribeResponseDTO toResponseDTO(Subscribe subscribe);
 }
