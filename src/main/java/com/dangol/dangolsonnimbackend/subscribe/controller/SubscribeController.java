b3ef901ef01ac3cc6cefa7afbf7d4f11612c39b1
Marine <rnjstmdals6@naver.com>
2023-03-04 03:06:13 +0900

    Revert "feat: 구독권 생성 api 추가"
    This reverts commit f1fceba8f4046052775f862096aa133c84aa5207.

--- src/main/java/com/dangol/dangolsonnimbackend/subscribe/controller/SubscribeController.java
+++ src/main/java/com/dangol/dangolsonnimbackend/subscribe/controller/SubscribeController.java
@@ -1,29 +1,9 @@
 package com.dangol.dangolsonnimbackend.subscribe.controller;
 
-import com.dangol.dangolsonnimbackend.subscribe.dto.SubscribeRequestDTO;
-import com.dangol.dangolsonnimbackend.subscribe.dto.SubscribeResponseDTO;
-import com.dangol.dangolsonnimbackend.subscribe.service.SubscribeService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/subscribe")
 public class SubscribeController {
-
-    private final SubscribeService subscribeService;
-
-    public SubscribeController(SubscribeService subscribeService) {
-        this.subscribeService = subscribeService;
-    }
-
-    @PostMapping
-    public ResponseEntity<SubscribeResponseDTO> create(@AuthenticationPrincipal String email,
-                                                       @Valid @RequestBody SubscribeRequestDTO requestDTO){
-        SubscribeResponseDTO responseDTO = subscribeService.create(requestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
-    }
 }
