3a0bdea25cec32a4ea655cc523ccb63b7d26b785
Marine <rnjstmdals6@naver.com>
2023-03-07 02:35:39 +0900

    refact: 필요 없는 코드 제거

--- src/test/java/com/dangol/dangolsonnimbackend/boss/controller/EmailControllerTest.java
+++ src/test/java/com/dangol/dangolsonnimbackend/boss/controller/EmailControllerTest.java
@@ -55,7 +55,5 @@ class EmailControllerTest {
                         .param("email", email))
                 // then
                 .andExpect(status().isBadRequest());
-
-        long endTime = System.currentTimeMillis();
     }
 }
