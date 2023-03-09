bb7673b848c52149834198e3aea3cc2fb52865c4
Hyeonji Lee <zcoioo12@gmail.com>
2023-03-10 00:26:57 +0900

    feat: Implement securityFilterChain

--- src/main/java/com/dangol/dangolsonnimbackend/config/SecurityConfig.java
+++ src/main/java/com/dangol/dangolsonnimbackend/config/SecurityConfig.java
@@ -1,14 +1,33 @@
 package com.dangol.dangolsonnimbackend.config;
 
+import com.dangol.dangolsonnimbackend.config.jwt.TokenProvider;
+import com.dangol.dangolsonnimbackend.customer.repository.CustomerRepository;
+import com.dangol.dangolsonnimbackend.oauth2.handler.OAuth2LoginFailHandler;
+import com.dangol.dangolsonnimbackend.oauth2.handler.OAuth2LoginSuccessHandler;
+import com.dangol.dangolsonnimbackend.oauth2.service.CustomOAuth2UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final CustomerRepository customerRepository;
+    private final ObjectMapper objectMapper;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginFailHandler oAuth2LoginFailHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -23,4 +42,36 @@ public class SecurityConfig {
                 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
         return http.build();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .httpBasic().disable()
+                .headers().frameOptions().disable()
+                .and()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+
+                .authorizeRequests()
+
+                .antMatchers("/login/profile").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/favicon.ico").permitAll()
+                .anyRequest().authenticated()
+                .and()
+
+                //== 소셜 로그인 설정 ==//
+                .oauth2Login()
+                .successHandler(oAuth2LoginSuccessHandler)
+                .failureHandler(oAuth2LoginFailHandler)
+                .userInfoEndpoint().userService(customOAuth2UserService);
+
+        return http.build();
+    }
+
+
 }
