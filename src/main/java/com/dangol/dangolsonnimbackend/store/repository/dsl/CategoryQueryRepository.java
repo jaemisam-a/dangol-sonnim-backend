37fd39dfa88ffe2dcc3fce2f15c70ae20c8e0c31
y00njinuk <71953982+y00njinuk@users.noreply.github.com>
2023-03-17 01:24:13 +0900

    feat: add Category repository
    [issue-key] DS-64
    - JPQL 기반의 Repoistory 클래스 정의
    - QueryDSL 기반의 Repoistory 클래스 정의

---
+++ src/main/java/com/dangol/dangolsonnimbackend/store/repository/dsl/CategoryQueryRepository.java
@@ -0,0 +1,45 @@
+package com.dangol.dangolsonnimbackend.store.repository.dsl;
+
+import com.dangol.dangolsonnimbackend.store.domain.Category;
+import com.dangol.dangolsonnimbackend.store.domain.QCategory;
+import com.dangol.dangolsonnimbackend.store.domain.Store;
+import com.dangol.dangolsonnimbackend.store.enumeration.CategoryType;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class CategoryQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    /**
+     * 카테고리 ID 값을 통해 카테고리 정보를 조회한다.
+     *
+     * @param id 카테고리 ID
+     * @return 카테고리 엔티티 객체
+     */
+    public Optional<Category> findById(Long id) {
+        return Optional.ofNullable(
+                queryFactory.select(QCategory.category)
+                        .where(QCategory.category.id.eq(id))
+                        .fetchOne());
+    }
+
+    /**
+     * 카테고리 코드 값을 통해 가게 목록을 조회한다.
+     *
+     * @param categoryType 카테고리 ID
+     * @return 카테고리 엔티티 객체
+     */
+    public Optional<Category> findByCategoryType(CategoryType categoryType) {
+        return Optional.ofNullable(
+                queryFactory.selectFrom(QCategory.category)
+                        .where(QCategory.category.categoryType.eq(categoryType))
+                        .fetchOne());
+    }
+}
