fe205247a58fc440b046bf7fb3114b783e3f9eba
Marine <rnjstmdals6@naver.com>
2023-03-21 00:03:05 +0900

    feat: 구독권 엔티티와 DTO 레포지토리 추가

---
+++ src/main/java/com/dangol/dangolsonnimbackend/subscribe/dto/BenefitDto.java
@@ -0,0 +1,14 @@
+package com.dangol.dangolsonnimbackend.subscribe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class BenefitDto {
+    private String description;
+}
