5f20d738df2550a7bb9af347022de2dc0a4a6989
Marine <rnjstmdals6@naver.com>
2023-04-06 17:49:43 +0900

    feat: 메뉴 관련 에러메세지 추가

--- src/main/java/com/dangol/dangolsonnimbackend/errors/enumeration/ErrorCodeMessage.java
+++ src/main/java/com/dangol/dangolsonnimbackend/errors/enumeration/ErrorCodeMessage.java
@@ -24,7 +24,8 @@ public enum ErrorCodeMessage {
     RESPONSE_CREATE_ERROR("응답 데이터를 생성하던 도중 에러가 발생하였습니다."),
     EMAIL_SEND_FAILURE("이메일 전송에 실패하였습니다."),
     FILE_UPLOAD_FAIL("파일 업로드에 실패했습니다."),
-    FILE_DELETE_FAIL("파일 삭제에 실패했습니다.");
+    FILE_DELETE_FAIL("파일 삭제에 실패했습니다."),
+    MENU_NOT_FOUND("존재하지 않는 메뉴입니다.");
 
     private final String message;
 }
