c45bd3d5553be2f73322a30c6177f5cf103cf906
Marine <rnjstmdals6@naver.com>
2023-05-16 22:30:26 +0900

    feat: cors 설정 추가

--- src/main/java/com/dangol/dangolsonnimbackend/config/WebMvcConfig.java
+++ src/main/java/com/dangol/dangolsonnimbackend/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.dangol.dangolsonnimbackend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,9 +10,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private static final long MAX_AGE_SECS = 3600;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/docs/**")
                 .addResourceLocations("classpath:/static/docs/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 대해
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Set-Cookie")
+                .maxAge(MAX_AGE_SECS);
     }
 }
